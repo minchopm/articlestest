631f79fa371297fdc08c6059f2a4492be71459b8
mincho.milev <mincho.milev@gmail.com>
2021-07-29 11:12:35 +0300

    fixes

--- src/components/SingleArticle/SingleArticle.js
+++ src/components/SingleArticle/SingleArticle.js
@@ -37,30 +37,27 @@ class SingleArticle extends Component {
                 <h1 style={{fontWeight: 300}}>Articles Listing</h1>
                 <h2>Articles Details</h2>
                 {this.state.article && 
-                <div style={{textAlign: 'left', maxWidth: 600, margin: 'auto'}}>
+                <div className="singleArticle">
 
-                    <h4 style={{color: '#4EAACD', margin: '10px 0', cursor: 'pointer', width: 'fit-content'}}>{this.state.article.titleEN}</h4>
+                    <h4>{this.state.article.titleEN}</h4>
                     <div style={{marginBottom: 5}}>{this.state.article.descriptionEN}</div>
-                    <div style={{fontSize: 14, color: 'grey'}}>{moment(this.state.article.publishedAt).format("MMMM DD, YYYY")}</div>
+                    <div className="publishedAt">{moment(this.state.article.publishedAt).format("MMMM DD, YYYY")}</div>
                 </div>}
-                <div style={{display: 'flex', alignItems: 'center', justifyContent: 'flex-start', maxWidth: 600, margin: 'auto'}}>
-                    <Dropdown.Divider style={{flex: '1 1 auto'}} />
-                    <a style={{textDecoration: 'none', marginLeft: 15, color: '#0d6efd', cursor: 'pointer'}}
-                        onClick={(event) => this.goToArticles(event)}>
+                <div className="singleArticleLinks back">
+                    <Dropdown.Divider className="divider" />
+                    <a onClick={(event) => this.goToArticles(event)}>
                         Back to the listing
                     </a>
                 </div>
-                <div style={{maxWidth: 600, display: 'flex', alignItems: 'center', justifyContent: 'flex-start', margin: 'auto'}}>
+                <div className="singleArticleLinks links">
                     {this.props.match.params.id > 1 && 
-                    <a style={{textDecoration: 'none', marginRight: 10, cursor: 'pointer', color: '#0d6efd'}} 
-                        onClick={(event) => this.previous(event)}>
-                            &lt; Previous
+                    <a onClick={(event) => this.previous(event)} className="previous">
+                        &lt; Previous
                     </a>}
                     {this.props.match.params.id > 1 && this.props.match.params.id < this.state.articles.length && <div>-</div>}
                     {this.props.match.params.id < this.state.articles.length && 
-                    <a style={{textDecoration: 'none', marginLeft: 10, cursor: 'pointer', color: '#0d6efd'}} 
-                        onClick={(event) => this.next(event)}>
-                            Next &gt;
+                    <a className="next" onClick={(event) => this.next(event)}>
+                        Next &gt;
                     </a>}
                 </div>
             </div>
