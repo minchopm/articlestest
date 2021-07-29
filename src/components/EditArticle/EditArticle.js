0bfbdbe54849becb505cd7871e0491f5ccf35b22
mincho.milev <mincho.milev@gmail.com>
2021-07-29 10:25:55 +0300

    formik

--- src/components/EditArticle/EditArticle.js
+++ src/components/EditArticle/EditArticle.js
@@ -2,57 +2,62 @@ import React, { Component } from "react";
 import { Tabs, Tab, Form, Button } from "react-bootstrap";
 import * as moment from 'moment';
 import { Editor, OriginalTools } from 'react-bootstrap-editor';
+import { Formik } from "formik";
 
 class EditArticle extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            id: null,
-            title: '',
-            description: '',
-            publishedAt: '',
-            isActive: false
         };
     }
 
     componentDidMount() {
+        
+    }
+
+    returnInitialValues = () => {
         if(this.props.match.params.id) {
             const articles = JSON.parse(localStorage.getItem('articles'));
             const foundArticle = articles[this.props.match.params.id-1];
-            this.setState({
-                id: foundArticle.id,
-                title: foundArticle.title,
-                description: foundArticle.description,
-                publishedAt: foundArticle.publishedAt,
-                isActive: foundArticle.isActive
-            });
+            return {
+                    id: foundArticle.id,
+                    title: foundArticle.title,
+                    description: foundArticle.description,
+                    publishedAt: foundArticle.publishedAt,
+                    isActive: foundArticle.isActive
+            };
+        } else {
+            return {
+                id: null,
+                title: '',
+                description: '',
+                publishedAt: '',
+                isActive: false
+            }
         }
     }
 
-    submitForm = (event) => {
-        event.preventDefault();
+    submitForm = (formProps) => {
         const articles = JSON.parse(localStorage.getItem('articles'));
         if(this.props.match.params.id) {
             const foundArticle = articles[this.props.match.params.id-1];
-            foundArticle.title = this.state.title;
-            foundArticle.description = this.state.description;
-            foundArticle.publishedAt = this.state.publishedAt;
-            foundArticle.isActive = this.state.isActive;
-            localStorage.setItem('articles', JSON.stringify(articles));
-            this.props.history.push('/articles');
+            foundArticle.title = formProps.values.title;
+            foundArticle.description = formProps.values.description;
+            foundArticle.publishedAt = formProps.values.publishedAt;
+            foundArticle.isActive = formProps.values.isActive;
         } else {
             const newArticle = {
                 id: +articles[articles.length-1].id+1,
-                title: this.state.title,
-                description: this.state.description,
-                publishedAt: this.state.publishedAt,
-                isActive: this.state.isActive
+                title: formProps.values.title,
+                description: formProps.values.description,
+                publishedAt: formProps.values.publishedAt,
+                isActive: formProps.values.isActive
             };
             articles.push(newArticle);
-            localStorage.setItem('articles', JSON.stringify(articles));
-            this.props.history.push('/articles');
         }
+        localStorage.setItem('articles', JSON.stringify(articles));
+        this.props.history.push('/articles');
     }
 
     render() {
@@ -72,35 +77,42 @@ class EditArticle extends Component {
                         <Tab eventKey="bulgarian" title="Bulgarian">
                         </Tab>
                     </Tabs>
-                    <Form style={{textAlign: 'left'}} onSubmit={this.submitForm}>
-                        <Form.Group className="mb-3 otherGroup" controlId="exampleForm.ControlInput1">
-                            <Form.Label>
-                                Title <span className="red">*</span>
-                            </Form.Label>
-                            <Form.Control type="text" placeholder="Enter article title" onChange={(event) => this.setState({title: event.target.value})} 
-                            value={this.state.title} />
-                        </Form.Group>
-                        <Form.Group className="mb-3 contentGroup" controlId="exampleForm.ControlTextarea1">
-                            <Form.Label>Content <span className="red">*</span></Form.Label>
-                            <Editor
-                                tools={OriginalTools}
-                                value={this.state.description}
-                                onChange={(value) => this.setState({description: value})}
-                            />
-                        </Form.Group>
-                        <Form.Group className="mb-3 otherGroup" controlId="formBasicCheckbox">
-                            <Form.Label>
-                                Date <span className="red">*</span>
-                            </Form.Label>
-                            <Form.Control type="date" name='date_of_birth' onChange={(event) => this.setState({publishedAt: event.target.value})} value={this.state.publishedAt} />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                            <Form.Check type="checkbox" label="Is Active" onChange={() => this.setState({isActive: !this.state.isActive})} checked={this.state.isActive} />
-                        </Form.Group>
-                        <Button variant="primary" type="submit">
-                            Submit
-                        </Button>
-                    </Form>
+                    <Formik initialValues={this.returnInitialValues()}>
+                        {(formProps) => (
+                            <Form className="py-3">
+                                <Form.Group className="mb-3 otherGroup" controlId="exampleForm.ControlInput1">
+                                    <Form.Label>
+                                        Title <span className="red">*</span>
+                                    </Form.Label>
+                                    <Form.Control type="text" placeholder="Enter article title" name="title" onChange={(event) => {
+                                        formProps.setFieldValue('title', event.target.value);
+                                    }} value={formProps.values.title} />
+                                </Form.Group>
+                                <Form.Group className="mb-3 contentGroup" controlId="exampleForm.ControlTextarea1">
+                                    <Form.Label>Content <span className="red">*</span></Form.Label>
+                                    <Editor
+                                        tools={OriginalTools}
+                                        value={formProps.values.description}
+                                        onChange={(value) => formProps.setFieldValue('description', value)}
+                                    />
+                                </Form.Group>
+                                <Form.Group className="mb-3 otherGroup" controlId="formBasicCheckbox">
+                                    <Form.Label>
+                                        Date <span className="red">*</span>
+                                    </Form.Label>
+                                    <Form.Control type="date" name='publishedAt' onChange={(event) => formProps.setFieldValue('publishedAt', event.target.value)} 
+                                        value={formProps.values.publishedAt} />
+                                </Form.Group>
+                                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                                    <Form.Check type="checkbox" label="Is Active" name="isActive" onChange={() => formProps.setFieldValue('isActive', !formProps.values.isActive)} checked={formProps.values.isActive} />
+                                </Form.Group>
+                                <Button variant="primary" onClick={() => this.submitForm(formProps)} disabled={!formProps.values.title || !formProps.values.description || !formProps.values.publishedAt}>
+                                    Submit
+                                </Button>
+                            </Form>
+                        )}
+                        
+                    </Formik>
                 </div>
             </div>
         );
